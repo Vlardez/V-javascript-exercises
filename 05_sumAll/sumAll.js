9e12f9651a844cd6e78752e6ab2026b5d066ed7f
Vlardez <m.vlad.popa@gmail.com>
2022-06-22 22:23:02 -0400

    finished exercieses 1-5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a,b) {
+    if (a<0 || b<0 || typeof(a) !== 'number' || typeof(b) !== 'number') {
+        return 'ERROR';
+    }
+    let sum = 0;
+    if (b>a) {
+        for (let i = a; i<=b; i++) {
+            sum += i;
+        };
+     } else if (a>b) {
+         for (let i = b; i<=a; i++) {
+             sum += i;
+         };
+     };
+     return sum;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
