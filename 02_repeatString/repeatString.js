20ad30bafed8b9629d0ae4853b374dd3d914f267
Vlardez <m.vlad.popa@gmail.com>
2022-06-22 22:24:22 -0400

    ok, actually adding in exercises 1-5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    const output = [];
+    if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+        output.push(str);
+    };
+    return output.join('');
+} else {
+    return 'ERROR';
+}
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arr,...elements) {
+    let i;
+    elements.forEach(element => {
+        i = arr.indexOf(element);
+        if (i >= 0) {
+            arr.splice(i,1);
+        }
+    });
+    return arr;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
