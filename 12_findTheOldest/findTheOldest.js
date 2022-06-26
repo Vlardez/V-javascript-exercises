d44ee2d05251e4600ed4a9ee2e380c5b49741e51
Vlardez <m.vlad.popa@gmail.com>
2022-06-26 15:25:12 -0400

    finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const ages = people.map((e) => {
+        if ('yearOfDeath' in e) {
+            return e.yearOfDeath - e.yearOfBirth;
+        } else {
+            const d = new Date();
+            return d.getFullYear() - e.yearOfBirth;
+        }
+    });
+    const oldest = ages.indexOf(Math.max(...ages));
+    return people[oldest];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
