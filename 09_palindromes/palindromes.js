22c3ec4d123f55fcbe60763797486d2a561693e2
Vlardez <m.vlad.popa@gmail.com>
2022-06-26 14:07:01 -0400

    Finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const inStr = str.replace(/[^A-Za-z]/g,'').toLowerCase();
+    const revStr = inStr.split('').reverse().join('');
+    return (inStr === revStr);
 };
-
 // Do not edit below this line
 module.exports = palindromes;
