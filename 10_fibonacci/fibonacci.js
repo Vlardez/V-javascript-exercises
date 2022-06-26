780b7fc250d275b66c61f34113c38f63ec9b6d0d
Vlardez <m.vlad.popa@gmail.com>
2022-06-26 14:24:35 -0400

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let x;
+    // changes a string number to an integer
+    if (typeof input === 'string') {
+        x = parseInt(input);
+    } else {
+        x = input;
+    };
+    const fib = [0,1];
+    if (x >= 0) {
+        for (let i = 2; i <=x; i++) {
+            fib[i] = fib[i-2] + fib[i-1];
+        };
+        return fib[x];
+    } else {return "OOPS"}
 };
 
 // Do not edit below this line
